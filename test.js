bea6465085d5dd6d69dd56be193864229c403825
isaacs <i@izs.me>
2010-08-19 10:38:54 -0700

    reduced more

--- test.js
+++ test.js
@@ -1,18 +1,11 @@
 var nas = require("./build/default/nas")
-  , sys = require("sys")
+  , start = Date.now()
 
-if (typeof console === "undefined") {
-  global.console = { log : function () {
-    sys.debug(sys.inspect([].slice.call(arguments)))
-  }}
-}
-
-
-console.log(Date.now(), "before hello")
+console.log("js "+(Date.now() - start), "before hello")
 nas.doSomething(1, 2, "hello", function (er, res, n) {
-  console.log(Date.now(), er, res, n)
+  console.log("js "+(Date.now() - start), er, res, n)
 })
-console.log(Date.now(), "before goodbye")
-nas.doSomething(3, 4, "goodbye", function (er, res, n) {
-  console.log(Date.now(), er, res, n)
-})
+// console.log(Date.now(), "before goodbye")
+// nas.doSomething(3, 4, "goodbye", function (er, res, n) {
+//   console.log(Date.now(), er, res, n)
+// })
